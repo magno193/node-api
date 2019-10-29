81722abb820565030a1930c48ba0b82fc17e5aa1
Alexandre Ferreira <alexmlf93@gmail.com>
2019-10-29 14:34:44 -0300

    Conexão com o mongoDB Atlas

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express'); // Importa express pelo require
+const mongoose = require('mongoose') // Importa mongoose pelo require
 
 const app = express(); // Atribui a constante app a função express
+
+// Conexão com banco de dados mongoDB Atlas
+mongoose.connect('mongodb+srv://admin:admin@omnistack-ymbxy.mongodb.net/nodeapi?retryWrites=true&w=majority',{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
 
 // Rota raiz que retorna Hello Rockeatseat
 app.get('/', (request, response) => {
