cbca33877eb1a1d4ca580f67da2d3bac5bb15e8e
Alexandre Ferreira <alexmlf93@gmail.com>
2019-10-29 17:17:13 -0300

    Reestruturação de server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const requireDir = require('require-dir') // importa require-dir
 const app = express(); // Atribui a constante app a função express
 
 // Conexão com banco de dados mongoDB Atlas
-mongoose.connect('mongodb+srv://admin:admin@omnistack-ymbxy.mongodb.net/nodeapi?retryWrites=true&w=majority',{
+mongoose.connect(
+    'mongodb+srv://admin:admin@omnistack-ymbxy.mongodb.net/nodeapi?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
@@ -13,19 +14,7 @@ mongoose.connect('mongodb+srv://admin:admin@omnistack-ymbxy.mongodb.net/nodeapi?
 // Registrar todas as models
 requireDir('./src/models');
 
-// Instancia a model Product
-const Product = mongoose.model('Product');
-
-// Rota raiz que retorna Hello Rockeatseat e
-// cria um registo do model Product
-app.get('/', (request, response) => {
-    Product.create({
-        title: 'React Native',
-        description: 'Build native apps with React',
-        url: 'http://github.com/facebook/react-native'
-    })
-
-    return response.send("Hello Rocketseat")
-});
+// Rotas
+app.use('/api', require('./src/routes')); // Raiz
 
 app.listen(3001) // Utilizar a porta 3001 do navegador
