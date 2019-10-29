d3173fc0bad5c364c5fadc60ca83513f8822877c
Alexandre Ferreira <alexmlf93@gmail.com>
2019-10-29 10:35:37 -0300

    Configuração express

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express'); // Importa express pelo require
+
+const app = express(); // Atribui a constante app a função express
+
+app.listen(3001) // Utilizar a porta 3001 do navegador
