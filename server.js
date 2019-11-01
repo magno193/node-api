2b8ea273891d95349a0936d96ae4533195160a90
Alexandre Ferreira <alexmlf93@gmail.com>
2019-10-31 23:42:07 -0300

    Configurado cors

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const express = require('express'); // Importa express pelo require
-const mongoose = require('mongoose') // Importa mongoose pelo require
-const requireDir = require('require-dir') // importa require-dir
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose') 
+const requireDir = require('require-dir') 
 
 // Atribui a constante app a função express
 const app = express();
 // Permitir enviar dados para aplicação no formato JSON
 app.use(express.json());
+// Permitir o uso de cors liberando acesso para todos domínios
+app.use(cors());
 
 // Conexão com banco de dados mongoDB Atlas
 mongoose.connect(
